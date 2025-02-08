50c4012a4fb69a3aded4d0f7fac819593ce08648
yu-hasebe <yuhasebe0821@gmail.com>
2025-02-08 17:28:12 +0900

    feat: limit rate

--- api/main.go
+++ api/main.go
@@ -2,32 +2,104 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Background()
+type middleware struct {
+	redisClient *redis.Client
+}
 
-	rdb := redis.NewClient(&redis.Options{
+func (m *middleware) limitRate(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ip, err := getIP(r)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err := m.consumeToken(r.Context(), ip); err != nil {
+			w.WriteHeader(http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func (m *middleware) getTokens(ctx context.Context, ip string) int64 {
+	currentTime := time.Now().Unix()
+	lastRefilledTime, err := m.redisClient.HGet(ctx, ip, "last_refilled_time").Int64()
+	if err != nil && !errors.Is(err, redis.Nil) {
+		return 0
+	}
+	tokens, err := m.redisClient.HGet(ctx, ip, "tokens").Int64()
+	if err != nil && !errors.Is(err, redis.Nil) {
+		return 0
+	}
+	elapsedTime := currentTime - lastRefilledTime
+	if elapsedTime >= 10 {
+		tokens = 10
+	}
+	m.redisClient.HSet(ctx, ip, "last_refilled_time", currentTime, "tokens", tokens)
+	return tokens
+}
+
+func (m *middleware) consumeToken(ctx context.Context, ip string) error {
+	tokens := m.getTokens(ctx, ip)
+	if tokens <= 0 {
+		return errors.New("not enough tokens")
+	}
+	m.redisClient.HIncrBy(ctx, ip, "tokens", -1)
+	return nil
+}
+
+func getIP(r *http.Request) (string, error) {
+	ipAddresses := r.Header.Get("X-FORWARDED-FOR")
+	splitedIPs := strings.Split(ipAddresses, ",")
+	if len(splitedIPs) > 0 {
+		netIP := net.ParseIP(splitedIPs[len(splitedIPs)-1])
+		if netIP != nil {
+			return netIP.String(), nil
+		}
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		ip := netIP.String()
+		if ip == "::1" {
+			return "127.0.0.1", nil
+		}
+		return ip, nil
+	}
+
+	return "", errors.New("IP not found")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+}
+
+func main() {
+	redisClient := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 		PoolSize: 1000,
 	})
-	rdb.Set(ctx, "my-key", "my-value", 0)
-	result, err := rdb.Get(ctx, "my-key").Result()
-	if err != nil {
-		fmt.Fprintf(w, "error: %+v\n", err)
-		return
+	m := middleware{
+		redisClient: redisClient,
 	}
-
-	fmt.Fprintf(w, "hello world: %s\n", result)
-}
-
-func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", m.limitRate(handler))
 	http.ListenAndServe(":8080", nil)
 }
