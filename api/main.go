2bc8f1d7bc66546ade74f6109e204cc9229d86d9
yu-hasebe <yuhasebe0821@gmail.com>
2025-02-08 15:50:58 +0900

    feat: redis connection

--- api/main.go
+++ api/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello world!\n")
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+		PoolSize: 1000,
+	})
+	rdb.Set(ctx, "my-key", "my-value", 0)
+	result, err := rdb.Get(ctx, "my-key").Result()
+	if err != nil {
+		fmt.Fprintf(w, "error: %+v\n", err)
+		return
+	}
+
+	fmt.Fprintf(w, "hello world: %s\n", result)
 }
 
 func main() {
